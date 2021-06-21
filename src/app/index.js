df62741ffe8d05fae1bea46a83d870a3d72e8095
Zackary Santana <zackzackyack@gmail.com>
2021-06-21 18:58:00 -0400

    Added default filse

---
+++ src/app/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function PersonList() {
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/courses").then((res) => {
+      const data = res.data;
+      console.log(data);
+      setPersons(data);
+    });
+  }, [setPersons]);
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li key={person.UserID}>{person.Name + " " + person.Code}</li>
+      ))}
+    </ul>
+  );
+}
